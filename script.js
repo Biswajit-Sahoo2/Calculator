227c6b1b90e5b948ff11e1f9d03b2cfc2beb30fb
Biswajit Sahoo <121495932+Biswajit-Sahoo2@users.noreply.github.com>
2024-03-12 18:31:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,18 @@ let arr = Array.from(buttons);
 
 arr.forEach(button => {
     button.addEventListener('click', (e) => {
+        //If equal button is clicked evaluate the string
         if (e.target.innerHTML == "=") {
             string = eval(string);
             input.value = string;
         }
         else if (e.target.innerHTML == "AC") {
+            //If AC button is clicked clear the string
             string = "";
             input.value = string;
         }
         else if (e.target.innerHTML == "DEL") {
+            //If DEL button is clicked then delete the latest entered value
             string = string.substring(0,string.length-1);
             input.value = string;
         }
